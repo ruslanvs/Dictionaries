726a84ef010f1447c4213d5340061537a57db88c
Ruslan <ruslanvs@yahoo.com>
2018-01-09 18:04:33 -0800

    Assignment complete

---
+++ Dictionaries.py
@@ -0,0 +1,9 @@
+dictionary = {"name": "Eon the Star", "age": 101, "countryBirth": "Atlantida", "favLanguage": "Atlantic"}
+
+def printBio( incomingDictionary ):
+    print "My name is", incomingDictionary["name"]
+    print "My age is", incomingDictionary["age"]
+    print "My country of birth is", incomingDictionary["countryBirth"]
+    print "My favorite language is", incomingDictionary["favLanguage"]
+
+printBio( dictionary )
